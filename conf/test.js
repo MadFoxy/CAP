9573c5dbf323fca439acf0c2ae48e3e1fa3b7a7c
leeshx <kkii_55@163.com>
2015-08-28 15:25:41 +0800

    add cset function

---
+++ conf/test.js
@@ -0,0 +1,31 @@
+/*
+    list = sourceData
+    ranges = [11,22,34]
+*/
+function computeDataSet(list,ranges) {
+    var rts = [];
+    var object;
+    var rangeCount=0;
+    var NumH=0;
+    var NumL=1;
+    var MaxPrice=2;
+    var MaxBars=3;
+    var MinPrice=4;
+    var MinBars=5;
+    for(var i=0;i<list.size();i++){
+        object = list.get(i);
+        if(ranges[rangeCount]==(i+1)){
+            //println(typeof parseFloat(rangeCount));
+            rts.push({
+                NumH:NumH,
+                NumL:NumL,
+                MaxPrice:MaxPrice,
+                MaxBars:MaxBars,
+                MinPrice:MinPrice,
+                MinBars:MinBars
+            });
+            rangeCount++;
+        }
+    }
+    return rts;
+}
