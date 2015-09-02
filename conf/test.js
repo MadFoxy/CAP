34f5e9c772d9b8fb5757ca8b63586b737449f83c
leeshx <kkii_55@163.com>
2015-09-02 09:51:34 +0800

    update js

--- conf/test.js
+++ conf/test.js
@@ -9,27 +9,25 @@ function computeDataSet(data,list,ranges) {
     var rangeCount=0;
     var NumUp=0;
     var NumDn=0;
-    var MaxUpGap=0.0000;
-    var MaxUpPos=0;
-    var MaxDnGap=0.0000;
-    var MaxDnPos=0;
+    var MaxUpGapAndPos=[0.0000,0];
+    var MaxDnGapAndPos=[0.0000,0];
     //println(data[0]+'='+list.get(0)[0]+':'+list.size());
     for(var i=0;i<list.size();i++){
         object = list.get(i);
         NumUp = xNumUp(NumUp,data,object);
         NumDn = xNumDn(NumDn,data,object);
-        MaxUpGap = xMaxUpGap(MaxUpGap,data,object);
-        MaxUpPos = xMaxUpPos(MaxUpPos,data,object);
-        MaxDnGap = xMaxDnGap(MaxDnGap,data,object);
-        MaxDnPos = xMaxDnPos(MaxDnPos,data,object);
+        MaxUpGapAndPos = xMaxUpGapAndPos(MaxUpGapAndPos,data,object,i);
+        //MaxUpPos = xMaxUpPos(MaxUpPos,data,object,i);
+        MaxDnGapAndPos = xMaxDnGapAndPos(MaxDnGapAndPos,data,object,i);
+        //MaxDnPos = xMaxDnPos(MaxDnPos,data,object);
         if(ranges[rangeCount]==(i+1)){
             rts.push({
                 NumUp:NumUp,
                 NumDn:NumDn,
-                MaxUpGap:MaxUpGap,
-                MaxUpPos:MaxUpPos,
-                MaxDnGap:MaxDnGap,
-                MaxDnPos:MaxDnPos
+                MaxUpGap:MaxUpGapAndPos[0],
+                MaxUpPos:MaxUpGapAndPos[1],
+                MaxDnGap:MaxDnGapAndPos[0],
+                MaxDnPos:MaxDnGapAndPos[1]
             });
             rangeCount++;
         }
@@ -40,22 +38,34 @@ function computeDataSet(data,list,ranges) {
 * value = 需要返回的值
 * a = 当根行数据
 * b = 比较的行数据
+* Open  4
+  High  5
+  Low   6
+  Close 7
 * */
 function xNumUp(value,a,b){
-    return value+1;
+    if(parseFloat(a[5])<parseFloat(b[7])){
+       value = value+1;
+    }
+    return value;
 }
 function xNumDn(value,a,b){
-    return value+1;
+    if(parseFloat(a[6])>parseFloat(b[7])){
+        value = value+1;
+    }
+    return value;
 }
-function xMaxUpGap(value,a,b){
-    return value + parseFloat(a[6])+parseFloat(b[6]);
+function xMaxUpGapAndPos(value,a,b,i){
+    if(parseFloat(b[7]) - parseFloat(a[5])>value[0]){
+        value[0] = parseFloat(b[7]) - parseFloat(a[5]);
+        value[1] = i;
+    }
+    return value;
 }
-function xMaxUpPos(value,a,b){
-    return 2;
-}
-function xMaxDnGap(value,a,b){
-    return value + parseFloat(a[7])+parseFloat(b[7]);
-}
-function xMaxDnPos(value,a,b){
-    return 2;
+function xMaxDnGapAndPos(value,a,b,i){
+    if(parseFloat(a[6]) - parseFloat(b[7])>value[0]){
+        value[0] = parseFloat(b[7]) - parseFloat(a[5]);
+        value[1] = i;
+    }
+    return value;
 }
